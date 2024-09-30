7aecf6a7848050ac4a3a0c75c6206b6c3fbde7ae
premkumarelangovan <premkumar.elangovan@gmail.com>
2024-09-30 15:00:43 +0100

    version 4

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return "Hello World, from Flask!-v3"
+    return "Hello World, from Flask!-v4"
 
 
 if __name__ == "__main__":
